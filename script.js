8b7fb3aeea0328636da77134765c828f97497c5b
Eric Graves <graves.eric@gmail.com>
2023-01-01 12:21:28 -0600

    Added user input arrays

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
 // Assignment Code
+// arrays for user selectable password options
+const lettersUp = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lettersLo = lettersUp.toLocaleLowerCase();
+const numbers = "0123456789";
+const special = "!#$%&'()*+,-./:;<=>?@[]^_`{|}~";
+
+// console tests
+console.log(lettersLo);
+alert(lettersLo);
+console.log(special);
+alert(special);
+
+
+
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
@@ -7,6 +21,7 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
+  alert("Test")
 
 }
 
