01f74cd9e98b9d7769399cde4fb809a17300de70
Eric Graves <graves.eric@gmail.com>
2023-01-01 19:49:14 -0600

    added outputs to HTML and fixed some minor bugs

--- script.js
+++ script.js
@@ -3,35 +3,42 @@
 const lettersUp = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 const lettersLo = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 const numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
-const special = ['"', " ", "!", "#", "$", "%", "&", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{" , "|", "}", "~"];
+const special = ["!", "#", "$", "%", "&","*", "?", "@", "^"];
 
-//init password, array and object
+//init button, password, array and object
+var generateBtn = document.querySelector("#generate");
 var passArr = [];
-var passObj = {};
+let passObj = {};
 var passWord = [];
 
 // builds password object based on user responses
-function passParams() {
+function userQuery() {
+  passWord = [];
+  passObj = {};
+  console.log("Cleared on click:", passObj, passWord);
+  
+  
   passObj.count = prompt("How many chars?");
 
   if (passObj.count < 8 || passObj.count > 128 ) {
   alert("That number is not between 8 and 128.  Please enter a correct number.");
-  passParams();
+  userQuery();
     } else if (isNaN(passObj.count)) {
       alert("That is not a number.  Please enter a number between 8 and 128.")
-      passParams();
+      userQuery();
     }
     else {
       passObj.upper = confirm("Tall Letters?");
       passObj.lower = confirm("Small Letters?");
       passObj.nums = confirm("Numbers?");
       passObj.specChar = confirm("Wingdings?");
-      buildPass();
+      console.log("After Input:", passObj, passWord);
+      buildArray();
       }
 }
 
-//build password array based on user selections
-function buildPass() {
+//build password array based on user selections - SUPERFOLOUS REFACTOR LATER
+function buildArray() {
   if (passObj.upper == true) {
     passArr = passArr.concat(lettersUp);
   }
@@ -44,10 +51,11 @@ function buildPass() {
   if (passObj.specChar == true) {
     passArr = passArr.concat(special);
   }
-  passSelect()
+  passSelect();
+  console.log("buildArray:", passObj, passWord);
+  //outputs to password field
+  document.getElementById("password").innerHTML = passWord.join("");
 }
-
-passParams()
 
 //selects random components of the password array to make passWord 
 function passSelect() {
@@ -56,65 +64,42 @@ function passSelect() {
   }
 }
 
-//checks second array for matching items in first array
-function passComp(arr1, arr2) {
+//compares second array for matching items in first array
+function comp(arr1, arr2) {
   return arr1.some(item => arr2.includes(item))
 }
 
 //check for selected criteria and if not found reruns selection function and rechecks
 function passCheck() {
-  if (passObj.upper == true && passComp(lettersUp, passWord)) {
+  if (passObj.upper == true && comp(lettersUp, passWord)) {
     console.log("UPPER MATCH FOUND")
   }
-  if (passObj.upper == true && !passComp(lettersUp, passWord)) {
+  if (passObj.upper == true && !comp(lettersUp, passWord)) {
     passSelect()
     passCheck()
   }
-  if (passObj.lower == true && passComp(lettersLo, passWord)) {
+  if (passObj.lower == true && comp(lettersLo, passWord)) {
     console.log("LOWER MATCH FOUND")
   }
-  if (passObj.lower == true && !passComp(lettersLo, passWord)) {
+  if (passObj.lower == true && !comp(lettersLo, passWord)) {
     passSelect()
     passCheck()
   }
-  if (passObj.nums == true && passComp(numbers, passWord)) {
+  if (passObj.nums == true && comp(numbers, passWord)) {
     console.log("NUMBER MATCH FOUND")
   }
-  if (passObj.nums == true && !passComp(numbers, passWord)) {
+  if (passObj.nums == true && !comp(numbers, passWord)) {
     passSelect()
     passCheck()
   }
-  if (passObj.specChar == true && passComp(special, passWord)) {
+  if (passObj.specChar == true && comp(special, passWord)) {
     console.log("SPECIAL MATCH FOUND")
   }
-  if (passObj.specChar == true && !passComp(special, passWord)) {
+  if (passObj.specChar == true && !comp(special, passWord)) {
     passSelect()
     passCheck()
   }
 }
 
-
-
-console.log(passObj);
-console.log(passArr);
-console.log(passObj.count);
-console.log(passWord);
-console.log(passCheck())
-
-
-
-// var generateBtn = document.querySelector("#generate");
-
-// // Write password to the #password input
-// function writePassword() {
-//   var password = generatePassword();
-//   var passwordText = document.querySelector("#password");
-
-//   passwordText.value = password
-//   alert("Test");
-
-// }
-
-// // Add event listener to generate button
-// generateBtn.addEventListener("click", writePassword);
-
+//starts the query on click
+generateBtn.addEventListener("click", userQuery);
